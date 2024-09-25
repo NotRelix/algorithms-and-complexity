8499b1229111ff00705b7d7533f9f82aa5603f34
Reece Lim <lim.reecesergei@gmail.com>
2024-09-25 23:10:54 +0800

    add selection sort

--- sorting-part1.c
+++ sorting-part1.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 21
 
-int* selectionSort(int arr[], int size);
+void selectionSort(int arr[], int size);
+
+void swap(int* x, int* y);
 void printArray(int arr[], int size);
 
 int main() {
-    int arr[] = {2, 6, 9, 10, 3, 31, 1, 16, 21, 49, 28, 30, 9, 5, 11, 2, 32, 24, 27, 6, 4};
-    int arrLen = sizeof(arr) / sizeof(arr[0]);
+    int selectionArr[MAX] = {2, 6, 9, 10, 3, 31, 1, 16, 21, 49, 28, 30, 9, 5, 11, 2, 32, 24, 27, 6, 4};
 
     printf("Selection Sort:\n");
-    int* selectionArr = selectionSort(arr, arrLen);
-    printArray(selectionArr, arrLen);
+    selectionSort(selectionArr, MAX);
+    printArray(selectionArr, MAX);
 }
 
-int* selectionSort(int arr[], int size) {
-    
+void selectionSort(int arr[], int size) {
+    for (int x = 0; x < size; x++) {
+        int min = x;
+        for (int y = x + 1; y < size; y++) {
+            if (arr[min] > arr[y]) {
+                min = y;
+            }
+        }
+        swap(&arr[min], &arr[x]);
+    }
+}
+
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 void printArray(int arr[], int size) {
-    int x;
-    for (x = 0; x < size; x++) {
+    for (int x = 0; x < size; x++) {
         printf("%d ", arr[x]);
     }
     printf("\n");
