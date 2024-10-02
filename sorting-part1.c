23e72a1e19b13f8ac7ff1f14543b910b79398941
Reece Lim <lim.reecesergei@gmail.com>
2024-10-03 00:26:02 +0800

    add tournament sort

--- sorting-part1.c
+++ sorting-part1.c
@@ -9,6 +9,11 @@ void insertionSort(int arr[], int size);
 void shellSort(int arr[], int size);
 void heapSort(int arr[], int size);
 void heapify(int arr[], int size, int root);
+void tournamentSort(int arr[], int size);
+void createTree(int arr[], int tree[], int size, int* value);
+void recreate(int tree[], int size, int* value);
+int winner(int pos1, int pos2, int tree[], int size);
+
 
 void swap(int* x, int* y);
 void printArray(int arr[], int size);
@@ -43,6 +48,11 @@ int main() {
     printf("\nHeap Sort:\n");
     heapSort(heapArr, MAX);
     printArray(heapArr, MAX);
+
+    int tournamentArr[MAX] = {2, 6, 9, 10, 3, 31, 1, 16, 21, 49, 28, 30, 9, 5, 11, 2, 32, 24, 27, 6, 4};
+    printf("\nTournament Sort:\n");
+    tournamentSort(tournamentArr, MAX);
+    printArray(tournamentArr, MAX);
 }
 
 void selectionSort(int arr[], int size) {
@@ -133,6 +143,51 @@ void heapify(int arr[], int size, int root) {
     if (largest != root) {
         swap(&arr[largest], &arr[root]);
         heapify(arr, size, largest);
+    }
+}
+
+int winner(int pos1, int pos2, int tree[], int size) {
+    int x = pos1 >= size ? pos1 : tree[pos1];
+    int y = pos2 >= size ? pos2 : tree[pos2];
+    return tree[x] < tree[y] ? x : y;
+}
+
+void recreate(int tree[], int size, int* value) {
+    int i = tree[1];
+    while (i > 1) {
+        int j, k = i / 2;
+        if (i % 2 == 0 && i < 2 * size - 1) {
+            j = i + 1;
+        } else {
+            j = i - 1;
+        }
+        tree[k] = winner(i, j, tree, size);
+        i = k;
+    }
+    *value = tree[tree[1]];
+    tree[tree[1]] = INT_MAX;
+}
+
+void createTree(int arr[], int tree[], int size, int* value) {
+    for (int i = 0; i < size; i++) {
+        tree[i + size] = arr[i];
+    }
+    for (int i = 2 * size - 1; i > 1; i -= 2) {
+        int k = i / 2;
+        int j = i - 1;
+        tree[k] = winner(i, j, tree, size);
+    }
+    *value = tree[tree[1]];
+    tree[tree[1]] = INT_MAX;
+}
+
+void tournamentSort(int arr[], int size) {
+    int tree[2 * size];
+    int value;
+    createTree(arr, tree, size, &value);
+    for (int i = 0; i < size; i++) {
+        arr[i] = value;
+        recreate(tree, size, &value);
     }
 }
 
