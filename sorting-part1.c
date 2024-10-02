db5442dadb6def0f52e2d2a12a7a80aaeaad9baa
Reece Lim <lim.reecesergei@gmail.com>
2024-10-02 21:18:56 +0800

    add bubble sort

--- sorting-part1.c
+++ sorting-part1.c
@@ -3,16 +3,21 @@
 #define MAX 21
 
 void selectionSort(int arr[], int size);
+void bubbleSort(int arr[], int size);
 
 void swap(int* x, int* y);
 void printArray(int arr[], int size);
 
 int main() {
     int selectionArr[MAX] = {2, 6, 9, 10, 3, 31, 1, 16, 21, 49, 28, 30, 9, 5, 11, 2, 32, 24, 27, 6, 4};
-
     printf("Selection Sort:\n");
     selectionSort(selectionArr, MAX);
     printArray(selectionArr, MAX);
+
+    int bubbleArr[MAX] = {2, 6, 9, 10, 3, 31, 1, 16, 21, 49, 28, 30, 9, 5, 11, 2, 32, 24, 27, 6, 4};
+    printf("\nBubble Sort:\n");
+    bubbleSort(bubbleArr, MAX);
+    printArray(bubbleArr, MAX);
 }
 
 void selectionSort(int arr[], int size) {
@@ -24,6 +29,16 @@ void selectionSort(int arr[], int size) {
             }
         }
         swap(&arr[min], &arr[x]);
+    }
+}
+
+void bubbleSort(int arr[], int size) {
+    for (int x = size - 1; x > 0; x--) {
+        for (int y = 1; y <= x; y++) {
+            if (arr[y] < arr[y - 1]) {
+                swap(&arr[y], &arr[y - 1]);
+            }
+        }
     }
 }
 
