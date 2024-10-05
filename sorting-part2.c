fcd7a8971de8a426edd68a83f2f744266ecb50bc
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-10-05 17:13:20 +0800

    add quicksort lomuto's partition

--- sorting-part2.c
+++ sorting-part2.c
@@ -6,6 +6,8 @@
 // Quicksort
 void quicksortHoare(int arr[], int left, int right);
 int partitionHoare(int arr[], int left, int right);
+void quicksortLomuto(int arr[], int left, int right);
+int partitionLomuto(int arr[], int left, int right);
 
 void printArray(int arr[], int size);
 void swap(int* x, int* y);
@@ -15,6 +17,11 @@ int main() {
     printf("Quicksort (Hoare):\n");
     quicksortHoare(hoareArr, 0, MAX - 1);
     printArray(hoareArr, MAX);
+
+    int lomutoArr[MAX] = {2, 6, 9, 10, 3, 31, 1, 16, 21, 49, 28, 30, 9, 5, 11, 2, 32, 24, 27, 6, 4};
+    printf("\nQuicksort (Lomuto):\n");
+    quicksortLomuto(lomutoArr, 0, MAX - 1);
+    printArray(lomutoArr, MAX);
 }
 
 void quicksortHoare(int arr[], int left, int right) {
@@ -41,6 +48,27 @@ int partitionHoare(int arr[], int left, int right) {
         }
         swap(&arr[j], &arr[i]);
     }
+}
+
+void quicksortLomuto(int arr[], int left, int right) {
+    if (left < right) {
+        int pivot = partitionLomuto(arr, left, right);
+        quicksortLomuto(arr, left, pivot - 1);
+        quicksortLomuto(arr, pivot + 1, right);
+    }
+}
+
+int partitionLomuto(int arr[], int left, int right) {
+    int i = left - 1;
+    for (int j = left; j < right; j++) {
+        if (arr[j] < arr[right]) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    i++;
+    swap(&arr[i], &arr[right]);
+    return i;
 }
 
 void printArray(int arr[], int size) {
